cb9e071f462bc59ad288a868415ceea8acc29ed7
ShubhamAdmin <38102134+adminvns@users.noreply.github.com>
2022-03-08 12:02:11 +0530

    first

---
+++ src/main/java/com/application/first_boot/Security.java
@@ -0,0 +1,14 @@
+//package com.application.first_boot;
+//
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+//public class Security  extends WebSecurityConfigurerAdapter {
+//
+//    @Override
+//    protected void configure(HttpSecurity security) throws Exception
+//    {
+//        security.httpBasic().disable();
+//        security.formLogin().disable();
+//    }
+//}
---
+++ src/test/java/com/application/first_boot/FirstBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.first_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
