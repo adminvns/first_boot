b66f0a35a9659cb72353682285de5edc04a2bb04
ShubhamAdmin <38102134+adminvns@users.noreply.github.com>
2022-03-09 00:12:19 +0530

    hide passwords

--- src/main/java/com/application/first_boot/student/Student.java
+++ src/main/java/com/application/first_boot/student/Student.java
@@ -1,8 +1,28 @@
 package com.application.first_boot.student;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+
+
+@Entity  //for hibernate
+@Table  //for table in DB
 public class Student {
+
+
+@Id
+@SequenceGenerator(
+        name = "student_sequence",
+        sequenceName = "student_sequence",
+        allocationSize = 1
+)
+
+@GeneratedValue(
+        strategy = GenerationType.SEQUENCE,
+        generator = "student_sequence"
+
+
+)
 private  Long id;
 private String name;
 private Integer age;
--- src/main/java/com/application/first_boot/student/StudentService.java
+++ src/main/java/com/application/first_boot/student/StudentService.java
@@ -1,20 +1,21 @@
 package com.application.first_boot.student;
 
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
 
-@Component
+@Service
 public class StudentService {
 
     public List<Student> getStudents() {
 
         return List.of(
 
-                new Student(1L, "Shubham Singh", 25, LocalDate.of(1997, Month.AUGUST, 15), "Shubham@gmail.com"
+                new Student(1L, "Shubham Singh", 25, LocalDate.of(1997, Month.AUGUST, 15), "test@gmail.com"
                 )
         );
 
